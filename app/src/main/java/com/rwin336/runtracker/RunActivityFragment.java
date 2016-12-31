467979a50f160d205726e0d77bdf3471f37d9b4b
rwin336 <rwin336@gmail.com>
2016-12-31 18:01:23 -0500

    Initial commit

---
+++ app/src/main/java/com/rwin336/runtracker/RunActivityFragment.java
@@ -0,0 +1,22 @@
+package com.rwin336.runtracker;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class RunActivityFragment extends Fragment {
+
+    public RunActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_run, container, false);
+    }
+}
