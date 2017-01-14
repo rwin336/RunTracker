472e95c377a422e84ac6bd09c8dffce8dfdbaa91
rwin336 <rwin336@gmail.com>
2017-01-14 15:52:39 -0500

    Provides UI support for GPS values

---
+++ app/src/main/java/com/rwin336/runtracker/Run.java
@@ -0,0 +1,47 @@
+package com.rwin336.runtracker;
+
+import java.util.Date;
+
+/*
+ * Copyright (C) 2017 rwin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Run {
+    private Date mStartDate;
+
+
+    public Run() {
+        mStartDate = new Date();
+    }
+
+    public Date getStartDate() {
+        return mStartDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        mStartDate = startDate;
+    }
+
+    public int getDurationSeconds(long endMillis) {
+        return (int)((endMillis - mStartDate.getTime()) / 1000);
+    }
+
+    public static String formatDuration(int durationSeconds) {
+        int seconds = durationSeconds % 60;
+        int mintues = ((durationSeconds - seconds) / 60) % 60;
+        int hours = (durationSeconds - (mintues * 60) - seconds) / 3600;
+        return String.format("%02d:%02d:%02d", hours, mintues, seconds);
+    }
+}
--- app/src/main/java/com/rwin336/runtracker/RunFragment.java
+++ app/src/main/java/com/rwin336/runtracker/RunFragment.java
@@ -1,5 +1,10 @@
 package com.rwin336.runtracker;
 
+import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.location.Location;
+import android.renderscript.Double2;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,15 +13,36 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class RunFragment extends Fragment {
+
+    private BroadcastReceiver mLocationReceiver = new LocationReceiver() {
+
+        @Override
+        protected void onLocationReceived(Context context, Location loc) {
+            mLastLocation = loc;
+            if (isVisible())
+                updateUI();
+        }
+
+        @Override
+        protected void onProviderEnabledChanged(boolean enabled) {
+            int toastText = enabled ? R.string.gps_enabled : R.string.gps_disabled;
+            Toast.makeText(getActivity(), toastText, Toast.LENGTH_LONG).show();
+        }
+    };
+
+
     private static final String TAG = "RunFragment";
 
     private RunManager mRunManager;
 
+    private Run mRun;
+    private Location mLastLocation;
     private Button mStartButton, mStopButton;
     private TextView mStartedTextView, mLatitudeTextView;
     private TextView mLongitudeTextView, mAltitudeTextView, mDurationTextView;
@@ -43,6 +69,7 @@ public class RunFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 mRunManager.startLocationUpdates();
+                mRun = new Run();
                 updateUI();
             }
         });
@@ -60,8 +87,36 @@ public class RunFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        getActivity().registerReceiver(mLocationReceiver,
+                new IntentFilter(RunManager.ACTION_LOCATION));
+
+    }
+
+    @Override
+    public void onStop() {
+        getActivity().unregisterReceiver(mLocationReceiver);
+        super.onStop();
+    }
+
     private void updateUI() {
         boolean started = mRunManager.isTrackingRun();
+
+        if (mRun != null) {
+            mStartedTextView.setText(mRun.getStartDate().toString());
+        }
+
+        int durationSeconds = 0;
+        if (mRun != null && mLastLocation != null) {
+            durationSeconds = mRun.getDurationSeconds(mLastLocation.getTime());
+            mLatitudeTextView.setText(Double.toString(mLastLocation.getLatitude()));
+            mLongitudeTextView.setText(Double.toString(mLastLocation.getLongitude()));
+            mAltitudeTextView.setText(Double.toString(mLastLocation.getAltitude()));
+        }
+        mDurationTextView.setText(Run.formatDuration(durationSeconds));
+
         mStartButton.setEnabled(!started);
         mStopButton.setEnabled(started);
     }
